97ac80286a7a7f9b76f0f2776a325341d61bd188
Sobirt <sobir.tokhtakhodzhaev@icf.com>
2025-12-15 12:02:58 -0600

    tailwind upgrade

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -18,7 +18,7 @@ export default function ProfilePage() {
   }, []);
   return (
     <>
-      <header className="bg-white shadow">
+      <header className="bg-white shadow-sm">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">Profile</h1>
         </div>
