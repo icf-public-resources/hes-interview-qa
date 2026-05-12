5d41fdef80fd97ef7a464a7ba3d5dbc1bebcbe3e
Sobirt <sobir.tokhtakhodzhaev@icf.com>
2026-05-12 12:12:06 -0500

    fixing issue with generating schema on deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo ">>> Running Prisma migrations..."
 # npx prisma migrate deploy
-npx prisma db push --skip-generate
+npx prisma db push
 
 echo ">>> Starting Next.js application..."
 exec npm start
