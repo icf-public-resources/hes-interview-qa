e84194d7004ef73f8657fed9b2d923109acc8edf
Sobirt <sobir.tokhtakhodzhaev@icf.com>
2026-05-12 12:58:10 -0500

    reverting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo ">>> Running Prisma migrations..."
 # npx prisma migrate deploy
-npx prisma db push
+npx prisma db push --skip-generate
 
 echo ">>> Starting Next.js application..."
 exec npm start
